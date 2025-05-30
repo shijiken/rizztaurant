389d5f4ced3936969f220aadd572ad1321c418a9
shijiken <sj315825@gmail.com>
2025-05-30 21:48:03 +0800

    Initial commit of app with 2 tabs

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,32 @@
+import { Image } from 'expo-image';
+import { StyleSheet, View } from 'react-native';
+
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
+
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
