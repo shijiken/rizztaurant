3a574c1582598e0b4e6f982bd34485133e91e3be
shijiken <sj315825@gmail.com>
2025-07-14 21:29:21 +0800

    Fixed app.config.js

--- app.config.js
+++ app.config.js
@@ -1,13 +1,60 @@
 // app.config.js
-import 'dotenv/config'; 
+import 'dotenv/config';
 
-export default ({ config }) => {
-  return {
-    ...config,
+export default ({ config }) => ({
+  ...config,
+  expo: {
+    ...config.expo,
+    name: "rizztaurant",
+    slug: "rizztaurant",
+    owner: "shijiken",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "rizztaurant",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/images/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      edgeToEdgeEnabled: true,
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: ["expo-router", "expo-secure-store"],
+    experiments: {
+      typedRoutes: true,
+    },
+    eas: {
+      projectId: "e8a097d2-b52d-438e-ad52-d4d43e170340",
+    },
+    updates: {
+      url: "https://u.expo.dev/e8a097d2-b52d-438e-ad52-d4d43e170340",
+      fallbackToCacheTimeout: 0,
+    },
+    runtimeVersion: {
+      policy: "sdkVersion",
+    },
     extra: {
       expoPublicSupabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
-        expoPublicSupabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+      expoPublicSupabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
       expoPublicGooglePlacesKey: process.env.EXPO_PUBLIC_GOOGLE_PLACES_API_KEY,
+      eas: {
+        projectId: "e8a097d2-b52d-438e-ad52-d4d43e170340",
+      },
     },
-  };
-};
+  },
+});
